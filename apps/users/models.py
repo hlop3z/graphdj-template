bbf2814e910fd5983647b37e34ea76b046e63253
hlop3z <23062270+hlop3z@users.noreply.github.com>
2021-08-23 12:56:38 -0500

    add user model & urls

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+# Create your models here.
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+from django.db import models
+from graphdj.models import GraphqlMixin
+
+ROLES = settings.GRAPHDJ.get("ROLES")
+DEFAULT_ROLE = settings.GRAPHDJ.get("DEFAULT_ROLE")
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=128, unique=True)
+    role = models.PositiveSmallIntegerField(
+        choices=ROLES.items(), blank=True, null=True, default=DEFAULT_ROLE
+    )
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = "user"
+
+    class Graphql(GraphqlMixin):
+        pass
